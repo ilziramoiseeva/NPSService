9e67829d9820b96b26af899df1518d22b0a30130
ilziramoiseeva <ilzirkam@gmail.com>
2020-10-25 01:14:53 +0300

    fixed #1

--- src/NPSService.java
+++ src/NPSService.java
@@ -1,29 +1,21 @@
 public class NPSService {
-    int promoters = 0;
-    int detractors = 0;
-    int count=0;
-    double nps;
-
-    public double nps(int[] votes) {
-
-
+    public double nps(int[] votes){
+        int promoters=0;
+        int detractors=0;
+        int count=0;
         for (int i = 0; i < votes.length; i++) {
             if (votes[i]>8 && votes[i]<=10) {
                 promoters++;
             }
-        }
-        for (int i = 0; i < votes.length; i++) {
             if (votes[i]>=0 && votes[i] < 7 ) {
                 detractors++;
             }
-        }
-            for (int i = 0; i < votes.length; i++) {
-                if(votes[i]>=0 && votes[i]<=10){
-                    count++;
+            if(votes[i]>=0 && votes[i]<=10){
+                count++;
                 }
             }
-
-        return nps = promoters * 100.0 / count - detractors * 100.0 / count;
+         double nps = promoters * 100.0 / count - detractors * 100.0 / count;
+        return nps;
 
     }
 }
